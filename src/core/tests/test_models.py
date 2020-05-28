9d92e93fdce2d200be933608bdb3eb38e9fc21ff
Zahedul Alam <z.alam015@gmail.com>
2020-05-28 14:58:07 +0600

    add test case for user moddel

--- src/core/tests/test_models.py
+++ src/core/tests/test_models.py
@@ -13,4 +13,4 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email)
-        self.assertEqual(user.check_password(password))
+        self.assertTrue(user.check_password(password))
