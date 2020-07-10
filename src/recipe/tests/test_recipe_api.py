a75901e515ad74114a153c297824eaef76cd1c8e
Zahedul Alam <z.alam015@gmail.com>
2020-07-10 12:56:04 +0600

    add recipe viewset

--- src/recipe/tests/test_recipe_api.py
+++ src/recipe/tests/test_recipe_api.py
@@ -55,7 +55,7 @@ class PrivateRecipeApiTests(TestCase):
 
         res = self.client.get(RECIPES_URL)
 
-        recipes = Recipe.objects.all().order_by('id')
+        recipes = Recipe.objects.all().order_by('-id')
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
--- src/recipe/urls.py
+++ src/recipe/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from recipe import views
+from . import views
 
 
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
